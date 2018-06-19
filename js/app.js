d9c904f365a0c65d8bbd53473c3f76810c5df050
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-06-19 10:49:15 -0400

    Built and tested page variables and event listener for submit input

--- js/app.js
+++ js/app.js
@@ -5,8 +5,16 @@ const colorPicked = document.querySelector("#colorPicker").value;
 const gridHeight = parseInt(document.querySelector("#inputHeight").value);
 const gridWidth = parseInt(document.querySelector("#inputWidth").value);
 
+const canvas = document.querySelector("table");
+
 // When size is submitted by the user, call makeGrid()
+// Target submit input element
+const submitGrid = document.querySelectorAll("input")[2];
+// Event listener calls makeGrid() fn on click event
+submitGrid.addEventListener("click", makeGrid());
 
 function makeGrid() {
-    // Your code goes here!
+    for (let i = 0; i <= gridHeight; i++) {
+
+    }
   }
