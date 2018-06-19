0b19b774202500e8341418c5676d5064b38be8e1
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-06-19 12:01:38 -0400

    Built and tested canvas creation functionality

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
-// Select color input
-const colorPicked = document.querySelector("#colorPicker").value;
-
-// Select size input
-const gridHeight = parseInt(document.querySelector("#inputHeight").value);
-const gridWidth = parseInt(document.querySelector("#inputWidth").value);
-
-const canvas = document.querySelector("table");
-
-// When size is submitted by the user, call makeGrid()
 // Target submit input element
 const submitGrid = document.querySelectorAll("input")[2];
+
 // Event listener calls makeGrid() fn on click event
-submitGrid.addEventListener("click", makeGrid());
+submitGrid.addEventListener("click", function makeGrid(event) {
+    let row, col;
+    // Select size input
+    row = parseInt(document.querySelector("#inputHeight").value);
+    col = parseInt(document.querySelector("#inputWidth").value);
 
-function makeGrid() {
-    for (let i = 0; i <= gridHeight; i++) {
+    // Construct table
+    const canvas = document.querySelector("table");
 
+    for (let i = 0; i < row; i++) {
+        let newRow = document.createElement("tr");
+        canvas.appendChild(newRow);
+        for (let i = 0; i < col; i++) {
+            let newCol = document.createElement("td");
+            newRow.appendChild(newCol);
+        }
     }
-  }
+    event.preventDefault();
+});
+
+// Select color input
+let colorPicked = document.querySelector("#colorPicker").value;
