7e01b617069acfb601f142cdbe690178a6b7efb6
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-06-19 20:06:20 -0400

    Added event listener to makeGrid() function.  Tested successfully.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 // Target submit input element
 const submitGrid = document.querySelectorAll("input")[2];
 
+// Construct table selector element
+const canvas = document.querySelector("#pixelCanvas");
+
 // Event listener calls makeGrid() fn on click event
 submitGrid.addEventListener("click", function makeGrid(event) {
     let row, col;
@@ -8,19 +11,18 @@ submitGrid.addEventListener("click", function makeGrid(event) {
     row = parseInt(document.querySelector("#inputHeight").value);
     col = parseInt(document.querySelector("#inputWidth").value);
 
-    // Construct table
-    const canvas = document.querySelector("table");
-
     for (let i = 0; i < row; i++) {
         let newRow = document.createElement("tr");
         canvas.appendChild(newRow);
         for (let i = 0; i < col; i++) {
             let newCol = document.createElement("td");
+            newCol.setAttribute("class", "pixel");
             newRow.appendChild(newCol);
+            newCol.addEventListener("click", function () {
+                let colorPicked = document.querySelector("#colorPicker").value;
+                this.style.backgroundColor = colorPicked;
+            });
         }
     }
     event.preventDefault();
 });
-
-// Select color input
-let colorPicked = document.querySelector("#colorPicker").value;
