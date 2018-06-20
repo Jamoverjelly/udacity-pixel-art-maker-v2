ced1b253a53ae05b9112b319a8f0f94a30d21802
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-06-20 15:47:03 -0400

    Added clearCanvas() function and refactored existing code.  Added <tbody></tbody> element to index.html to enable targeting of an HTML collection for clearing the canvas.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,60 @@
 // Target submit input element
-const submitGrid = document.querySelectorAll("input")[2];
+const submitGrid = document.querySelector("[type='submit']");
+// Event listener calls makeGrid() fn on click event
+submitGrid.addEventListener("click", makeGrid);
 
 // Construct table selector element
 const canvas = document.querySelector("#pixelCanvas");
 
-// Event listener calls makeGrid() fn on click event
-submitGrid.addEventListener("click", function makeGrid(event) {
-    let row, col;
-    // Select size input
-    row = parseInt(document.querySelector("#inputHeight").value);
-    col = parseInt(document.querySelector("#inputWidth").value);
+// Select color input
+const colorSelected = document.querySelector("#colorPicker");
+colorSelected.addEventListener("change", colorSelection);
 
+function colorSelection() {
+    let colorPicked = colorSelected.value;
+    canvas.style.color = colorPicked;
+    return colorPicked;
+}
+
+// Create interactive canvas grid
+function makeGrid(event) {    
+    // Add call to clearCanvas function
+    clearCanvas();
+    // Select size input
+    const row = parseInt(document.querySelector("#inputHeight").value);
+    const col = parseInt(document.querySelector("#inputWidth").value);
+    event.preventDefault();
     for (let i = 0; i < row; i++) {
-        let newRow = document.createElement("tr");
-        canvas.appendChild(newRow);
-        for (let i = 0; i < col; i++) {
-            let newCol = document.createElement("td");
-            newCol.setAttribute("class", "pixel");
-            newRow.appendChild(newCol);
-            newCol.addEventListener("click", function () {
-                let colorPicked = document.querySelector("#colorPicker").value;
-                this.style.backgroundColor = colorPicked;
-            });
+        let newRow = document.querySelector("#pixelCanvas").insertRow();
+        for (let j = 0; j < col; j++) {
+            newRow.insertCell();
         }
     }
-    event.preventDefault();
-});
+
+    document.getElementById('pixelCanvas').addEventListener("click", function(event) {
+        if(event.target && event.target.nodeName == "TD") {
+            event.target.style.backgroundColor = colorSelection();
+        }
+    });
+}
+
+function clearCanvas() {
+    let tblTarget = document.getElementById("pixelCanvas");
+    while (tblTarget.tBodies[0].hasChildNodes()) {
+        tblTarget.tBodies[0].removeChild(tblTarget.tBodies[0].lastChild);
+    }
+}
+
+// ======= Attribution =======
+// The final function clearCanvas() was encountered as a means of
+// passing the final test from the Pixel Art Project grader and ensuring
+// proper function of the canvas being cleared each time the user clicks
+// the submit button. This prevents the addition of extra table elements
+// being appended to the original table as a bug.
+
+// The code clearCanvas() is adapted from the help provided by Sudhindra A to
+// another student requesting help on passing the Pixel Art Grader tests in Udacity's
+// Knowledge Service: https://knowledge.udacity.com/questions/396
+// Referenced code can be found at this link: https://codepen.io/sudhindra/project/editor/ZeJgJa
+// Other refactored code was applied to ensure functionality of the app.
+// ======= End attribution =======
